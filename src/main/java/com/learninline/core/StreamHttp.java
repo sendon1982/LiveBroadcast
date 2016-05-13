50fbe7845b9f4bf6b613613286b9622fa08f16d8
mjiang <mjiang@viator.com>
2016-05-13 14:04:07 +1000

    Initial Commit

---
+++ src/main/java/com/learninline/core/StreamHttp.java
@@ -0,0 +1,36 @@
+package com.learninline.core;
+
+import uk.co.caprica.vlcj.player.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.headless.HeadlessMediaPlayer;
+
+/**
+ * Created by mjiang on 12/05/16.
+ */
+public class StreamHttp {
+
+    public static void main(String[] args) throws Exception {
+
+        String media = "https://www.youtube.com/watch?v=NilASeqRsjw";
+        String options = formatHttpStream("127.0.0.1", 5555);
+
+        System.out.println("Streaming '" + media + "' to '" + options + "'");
+
+        MediaPlayerFactory mediaPlayerFactory = new MediaPlayerFactory(args);
+        HeadlessMediaPlayer mediaPlayer = mediaPlayerFactory.newHeadlessMediaPlayer();
+        mediaPlayer.playMedia(media, options);
+
+        // Don't exit
+        Thread.currentThread().join();
+    }
+
+    private static String formatHttpStream(String serverAddress, int serverPort) {
+        StringBuilder sb = new StringBuilder(60);
+        sb.append(":sout=#duplicate{dst=std{access=http,mux=ts,");
+        sb.append("dst=");
+        sb.append(serverAddress);
+        sb.append(':');
+        sb.append(serverPort);
+        sb.append("}}");
+        return sb.toString();
+    }
+}
